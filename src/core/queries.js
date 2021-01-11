41983a215eecf7051eb2fdaaef10855de60c6241
Nitin Kumar <LEXIPOLLLC\nkumar@vdi-sierra-09.lexipolllc.local>
2021-01-11 12:20:29 +0530

    add and update project

--- src/core/queries.js
+++ src/core/queries.js
@@ -4,5 +4,30 @@ export const QUERY_PRODUCTS = gql `query{
     allProject{
         id
         name
+        addedDate
     }
 } `;
+
+export const MUTATION_ADD_PRODUCT = gql`
+    mutation createProject($name: String!) {
+        createProject (name: $name){
+            project {
+                id
+                name
+                addedDate
+            }
+        }
+    }
+    `;
+
+    export const MUTATION_UPDATE_PRODUCT = gql`
+    mutation updateProject($id:ID!, $name: String!) {
+        updateProject (id: $id, name: $name){
+            project {
+                id
+                name
+                addedDate
+            }
+        }
+    }
+    `;
