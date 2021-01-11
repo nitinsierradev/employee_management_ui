8961371129772542cdf85caf033e38a2a4392cfe
Nitin Kumar <LEXIPOLLLC\nkumar@vdi-sierra-09.lexipolllc.local>
2021-01-11 18:53:02 +0530

    create update project with multiple fields

--- src/components/products/productsList.js
+++ src/components/products/productsList.js
@@ -15,6 +15,8 @@ const ProductList = () => {
         title: "Add Product"
     }
 
+    const phases = { "PI": "Project Initiation", "PP": 'Project Planning', "PE": 'Project Execution', "PMC": 'Project Monitoring and Control', "PC": 'Project Closed' };
+
     const [objData, setData] = useState(initialVal);
     const [dataList, setDataList] = useState([]);
     const [createProject, projectData] = useMutation(MUTATION_ADD_PRODUCT);
@@ -28,29 +30,48 @@ const ProductList = () => {
         let title = action == "edit" ? "Edit Project" : "Add Project";
         let updatedObj = {
             isOpen: isOpen,
-            data: item,
+            data: { ...item },
             action: action,
             title: title
+        }
+        if(updatedObj.data.estimatedDate){
+            updatedObj.data.estimatedDate = new Date(updatedObj.data.estimatedDate);
         }
         setData(updatedObj);
         // setAction(action);
     }
 
-    const onChangeProduct = e => {
-        let data = objData.data;
+    const onChangeProject = e => {
+        let data = { ...objData.data };
         data[e.target.name] = e.target.value;
         setData(prevState => {
-            return { ...prevState, data:data }
-          });
-        
+            return { ...prevState, data: data }
+        });
+
+    }
+
+    const viewPhases = v => {
+        if (!v) {
+            return phases["PI"];
+        } else {
+
+            return phases[v]
+        }
     }
     const saveProduct = e => {
         e.preventDefault();
         let item = objData.data;
-        if(!item.id && item.name){
-           createProject({variables:{name: item.name}}).then(response => {
-            setDialog(false, 'read', {});
-               let createItem = response.data.createProject.project;
+        let reqData = { 
+            name: item.name, 
+            estimatedDate: item.estimatedDate, 
+            phase: item.phase, 
+            description: item.description 
+        }
+        if (!item.id && item.name) {
+            
+            createProject({ variables: reqData }).then(response => {
+                setDialog(false, 'read', {});
+                let createItem = response.data.createProject.project;
                 let tempItem = {
                     id: createItem.id,
                     name: createItem.name,
@@ -60,47 +81,70 @@ const ProductList = () => {
                 data.allProject.push(tempItem);
                 // setDataList(data.allProject);
             })
-           .catch(err =>{
-           //handle error
-          });
-           
-        } else if(item.id && item.name){
-            updateProject({variables:{id:item.id, name: item.name}}).then(response => {
-             setDialog(false, 'read', {});
+                .catch(err => {
+                    //handle error
+                });
+
+        } else if (item.id && item.name) {
+            reqData["id"] = item.id;
+            updateProject({ variables: reqData }).then(response => {
+                setDialog(false, 'read', {});
                 let updateItem = response.data.createProject.project;
-             })
-            .catch(err =>{
-            //handle error
-           });
-            
-         }
+            })
+                .catch(err => {
+                    //handle error
+                });
+
+        }
     }
-    if(!loading && !error && data && data.allProject){
-        setTimeout(()=>{
+    if (!loading && !error && data && data.allProject) {
+        setTimeout(() => {
             setDataList(data.allProject);
         }, 100)
-        
+
     }
     const manageDateFormat = d => {
+        if (!d) return "";
         let dt = new Date(d);
-        return dt.getDate() + "-" + (dt.getMonth() + 1) + "-" + dt.getFullYear();
+        let day = dt.getDate();
+        let month = (dt.getMonth() + 1);
+        if (day < 10) {
+            day = "0" + day
+        }
+        if (month < 10) {
+            month = "0" + month
+        }
+        return day + "-" + month + "-" + dt.getFullYear();
     }
     return (
         <Card>
             <div>
-                <Button intent="primary" icon="add" text="Add Product" className="float-right" onClick={() => setDialog(true, 'add', {})}/>
+                <Button intent="primary" icon="add" text="Add Product" className="float-right" onClick={() => setDialog(true, 'add', {})} />
             </div>
             <table class="bp3-html-table bp3-html-table-bordered bp3-html-table-condensed bp3-html-table-striped ">
                 <thead>
                     <tr>
                         <th>S.No.</th>
                         <th>Name</th>
+                        <th>Description</th>
+                        <th>Phase</th>
                         <th>Created Date</th>
+                        <th>Estimated Date</th>
+                        <th>Closed Date</th>
                         <th>Edit</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {dataList.map((item, idx) => <tr key={item.id}><td>{idx + 1}</td><td>{item.name}</td><td>{manageDateFormat(item.addedDate)}</td><td><Icon icon="edit" onClick={() => setDialog(true, "edit", item)} /></td></tr>)}
+                    {dataList.map((item, idx) => (<tr key={item.id}><td>{idx + 1}</td>
+                        <td>{item.name}</td>
+                        <td>{item.description}</td>
+                        <td>{viewPhases(item.phase)}</td>
+                        <td>{manageDateFormat(item.addedDate)}</td>
+                        <td>{manageDateFormat(item.estimatedDate)}</td>
+                        <td>{manageDateFormat(item.closedDate)}</td>
+                        <td><Icon icon="edit" onClick={() => setDialog(true, "edit", item)} /></td>
+                    </tr>
+                    ))}
                 </tbody>
             </table>
             <Dialog
@@ -111,16 +155,69 @@ const ProductList = () => {
                 isOpen={objData.isOpen}
             >
                 <div className={Classes.DIALOG_BODY}>
-                    <ul>
-                        <li>
-                            <label>Name</label> <input name="name" value={objData.data.name} onChange={onChangeProduct} />
-                        </li>
-                        
-                    </ul>
-                    <Button text="Save" onClick={saveProduct}/>
+                    <div class="bp3-form-group bp3-inline">
+                        <label className="bp3-label" for="name">
+                            Name
+                            <span className="bp3-text-muted">(required)</span>
+                        </label>
+                        <div className="bp3-form-content">
+                            <div className="bp3-input-group">
+
+                                <input id="name" name="name" type="text" className="bp3-input"
+                                    placeholder="Placeholder text" value={objData.data.name} onChange={onChangeProject} />
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="bp3-form-group bp3-inline">
+                        <label className="bp3-label" for="phase">
+                            Phase
+                            <span className="bp3-text-muted">(required)</span>
+                        </label>
+                        <div className="bp3-form-content">
+                            <div className="bp3-input-group">
+
+                                <select id="phase" name="phase" value={objData.data.phase} onChange={onChangeProject}>
+                                    <option value="PI">Project Initiation</option>
+                                    <option value="PP">Project Planning</option>
+                                    <option value="PE">Project Execution</option>
+                                    <option value="PMC">Project Monitoring and Control</option>
+                                    <option value="PC">Project Closed</option>
+                                </select>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="bp3-form-group bp3-inline">
+                        <label className="bp3-label" for="estimatedDate">
+                            Estimated Date
+                            <span className="bp3-text-muted">(required)</span>
+                        </label>
+                        <div className="bp3-form-content">
+                            <div className="bp3-input-group">
+                                <input id="estimatedDate" name="estimatedDate" type="date" className="bp3-input"
+                                    placeholder="Placeholder text" value={objData.data.estimatedDate} onChange={onChangeProject} />
+                            </div>
+                            <div className="bp3-form-helper-text">(mm/dd/yyyy)</div>
+                        </div>
+                    </div>
+                    <div class="bp3-form-group bp3-inline">
+                        <label className="bp3-label" for="description">
+                            Description
+                            <span className="bp3-text-muted">(required)</span>
+                        </label>
+                        <div className="bp3-form-content">
+                            <div className="bp3-input-group">
+                                <textarea id="description" name="description" value={objData.data.description} onChange={onChangeProject}></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <Button intent="primary" className="float-right" text="Save" onClick={saveProduct} />
+                    </div>
+
                 </div>
             </Dialog>
-
         </Card>
     )
 }
--- src/core/queries.js
+++ src/core/queries.js
@@ -5,24 +5,32 @@ export const QUERY_PRODUCTS = gql `query{
         id
         name
         addedDate
+        description
+        phase
+        estimatedDate
+        closedDate
     }
 } `;
 
 export const MUTATION_ADD_PRODUCT = gql`
-    mutation createProject($name: String!) {
-        createProject (name: $name){
+    mutation createProject($name: String!, $estimatedDate: String, $phase: String!, $description: String) {
+        createProject (name: $name, estimatedDate: $estimatedDate, phase: $phase, description: $description){
             project {
                 id
                 name
                 addedDate
+                description
+                phase
+                estimatedDate
+                closedDate
             }
         }
     }
     `;
 
     export const MUTATION_UPDATE_PRODUCT = gql`
-    mutation updateProject($id:ID!, $name: String!) {
-        updateProject (id: $id, name: $name){
+    mutation updateProject($id:ID!, $name: String!, $estimatedDate: String, $phase: String!, $description: String) {
+        updateProject (id: $id, name: $name, estimatedDate: $estimatedDate, phase: $phase, description: $description){
             project {
                 id
                 name
