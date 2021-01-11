319c1f7a5d253b5ec0eb25574f33a351037fff1f
localadmin <localadmin@vdi-sierra-07.lexipolllc.local>
2021-01-11 13:36:03 +0530

    local commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { Route , Switch,BrowserRouter} from 'react-router-dom'
+import { Route , Switch,BrowserRouter, Router} from 'react-router-dom'
 import UserInfo from './employee'
-import Nav from './navbar'
+import Nav from './navbar';
+import AddEmployee from './employee/addEmployee';
 const App=()=>{
   return (
     <BrowserRouter>
       <Nav />
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <UserInfo />
         </Route>
         <Route path="/project">
+          <AddEmployee />
+        </Route>
+        <Route exact path="/addEmployee">
+         <AddEmployee />
+        </Route>
+        <Route path="/addEmployee/:id">
+          <AddEmployee  />
         </Route>
       </Switch>
     </BrowserRouter>
--- src/employee/index.js
+++ src/employee/index.js
@@ -1,12 +1,23 @@
 // React from 'react';
 import { useQuery } from 'react-apollo';
 import { gql } from 'apollo-boost';
+import "@blueprintjs/table/lib/css/table.css";
+import { Button, Card } from "@blueprintjs/core";
+import { Link, useParams } from "react-router-dom";
+import { useState } from 'react';
 const QUERY_USERS = gql`
   query {
-    allEmployee {
+    allEmployee{
       id
       firstName
       lastName
+      email
+      empId
+      dob
+      mobileNo
+      project{
+          name
+      }
     }
 }
 `;
@@ -16,12 +27,37 @@ const UserInfo = () => {
   // at a specified interval
   const { loading,data }  = useQuery(QUERY_USERS);
 if (loading) return (<div>Loading......</div>);
-    return data.allEmployee.map(({ id, firstName, lastName }) => (
-        <div key={id}>
-        <p>
-            User - {id}: {firstName} {lastName}
-        </p>
-        </div>
-  ));
+return <Card>
+  <div align="right"><Button intent="primary" text="Add Employee"   /></div>
+  <table align='center' class="bp3-html-table bp3-html-table-bordered bp3-html-table-striped .modifier">
+<thead>
+  <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Email</th>
+    <th>EmpId</th>
+    <th>DOB</th>
+    <th>Mobile No</th>
+    <th>Project Name</th>
+    <th>Action</th>
+  </tr>
+</thead>
+  <tbody>
+    
+    {data.allEmployee.map((user) => (
+      <tr>
+        <td>{user.firstName}</td>
+        <td>{user.lastName}</td>
+        <td>{user.email}</td>
+        <td>{user.empId}</td>
+        <td>{user.dob}</td>
+        <td>{user.mobileNo}</td>
+        <td>{user.project.name}</td>
+        <td><Button intent="success" text="Edit" /></td>
+        </tr>
+      ))}
+  </tbody>
+</table>
+</Card>
 }
 export default UserInfo
